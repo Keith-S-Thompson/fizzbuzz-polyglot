3a493bb1dca5836d899e4ab425870a0736c6ec4e
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-04-10 15:50:31 -0700

    Lua

---
+++ fizzbuzz.lua
@@ -0,0 +1,13 @@
+#!/usr/bin/lua
+
+for i = 1, 100, 1 do
+    if i % 15 == 0 then
+        print("FizzBuzz");
+    elseif i % 3 == 0 then
+        print("Fizz");
+    elseif i % 5 == 0 then
+        print("Buzz");
+    else
+        print(i)
+    end
+end
