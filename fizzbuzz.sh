5be3cf9a9a4ebaa23fd5597b6ad8d418b5590634
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-04-10 17:05:59 -0700

    fizzbuzz.sh

---
+++ fizzbuzz.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+i=1
+while [ $i -le 100 ] ; do
+    if [ `expr $i % 15` -eq 0 ] ; then
+        echo FizzBuzz
+    elif [ `expr $i % 3` -eq 0 ] ; then
+        echo Fizz
+    elif [ `expr $i % 5` -eq 0 ] ; then
+        echo Buzz
+    else
+        echo $i
+    fi
+    i=`expr $i + 1`
+done
