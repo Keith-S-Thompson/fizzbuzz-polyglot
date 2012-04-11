d639c67ff1820479a9c992cd51a7ea6f54792de6
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-04-10 17:35:28 -0700

    Fortran

---
+++ fizzbuzz.f
@@ -0,0 +1,34 @@
+       program FizzBuzz
+
+       do 1 i = 1, 100
+
+       if (mod(i, 15) .eq. 0) then
+       print '(a)', 'FizzBuzz'
+       goto 1
+       end if
+
+       if (mod(i, 3) .eq. 0) then
+       print '(a)', 'Fizz'
+       goto 1
+       end if
+
+       if (mod(i, 5) .eq. 0) then
+       print '(a)', 'Buzz'
+       goto 1
+       end if
+
+       if (i .lt. 10) then
+       print '(i1)', i
+       goto 1
+       end if
+
+       if (i .lt. 100) then
+       print '(i2)', i
+       goto 1
+       end if
+
+       print '(i3)', i
+
+1      continue
+
+       end program FizzBuzz
