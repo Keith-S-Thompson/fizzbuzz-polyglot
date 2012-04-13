3381c24c10bb52326827e32b67f764f83bec5efe
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-04-13 14:40:39 -0700

    Make compatible with Python 3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,10 +2,10 @@
 
 for i in range(1,101):
     if i % 15 == 0:
-        print "FizzBuzz"
+        print("FizzBuzz")
     elif i % 3 == 0:
-        print "Fizz"
+        print("Fizz")
     elif i % 5 == 0:
-        print "Buzz"
+        print("Buzz")
     else:
-        print i
+        print(i)
