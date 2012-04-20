39b50f02ed4625484beb7f0551e554c88b9956fc
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-04-19 23:39:49 -0700

    Terser

--- fizzbuzz.pl
+++ fizzbuzz.pl
@@ -5,16 +5,9 @@ use warnings;
 use feature 'say';
 
 for (1..100) {
-    if ($_ % 15 == 0) {
-        say "FizzBuzz";
-    }
-    elsif ($_ % 3 == 0) {
-        say "Fizz";
-    }
-    elsif ($_ % 5 == 0) {
-        say "Buzz";
-    }
-    else {
-        say;
-    }
+    my $s = '';
+    $s .= 'Fizz' if $_ % 3 == 0;
+    $s .= 'Buzz' if $_ % 5 == 0;
+    $s .= $_     if $s eq '';
+    say $s;
 }
