c07af1e4dc1b6d445b7889de70f545f676fd2585
hoganlong <hoganlong@gmail.com>
2012-07-07 22:34:14 -0400

    One line linq

---
+++ fizzbuzz.linq1.cs
@@ -0,0 +1,15 @@
+using System;
+class FizzBuzz {
+    static void Main() {
+    	Console.WriteLine(
+			String.Join(
+				Environment.NewLine,
+				Enumerable.Range(1, 100)
+					.Select(n => n % 15 == 0 ? "FizzBuzz" 
+						        : n % 3 == 0 ? "Fizz" 
+								: n % 5 == 0 ? "Buzz" 
+								: n.ToString())
+			)
+		);
+    }
+}
