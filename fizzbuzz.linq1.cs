fe1610f04c2577a0e7fb413c99aac582de5a4248
hoganlong <hoganlong@gmail.com>
2012-07-07 23:21:36 -0400

    Tabs to spaces

--- fizzbuzz.linq1.cs
+++ fizzbuzz.linq1.cs
@@ -3,15 +3,14 @@ using System.Linq;
 
 class FizzBuzz {
     static void Main() {
-    	Console.WriteLine(
-			String.Join(
-				Environment.NewLine,
-				Enumerable.Range(1, 100)
-					.Select(n => n % 15 == 0 ? "FizzBuzz" 
-						        : n % 3 == 0 ? "Fizz" 
-								: n % 5 == 0 ? "Buzz" 
-								: n.ToString())
-			)
-		);
+      Console.WriteLine(
+      String.Join(
+        Environment.NewLine,
+        Enumerable.Range(1, 100)
+          .Select(n => n % 15 == 0 ? "FizzBuzz" 
+                     : n % 3 == 0 ? "Fizz" 
+                     : n % 5 == 0 ? "Buzz" 
+                     : n.ToString())
+      ));
     }
 }
