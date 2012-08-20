747b43266de7f32ec93c5c3bfd9f852c86be9537
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-08-19 20:34:49 -0700

    Add Visual Basic .NET

---
+++ fizzbuzz.vb
@@ -0,0 +1,19 @@
+' Language: Visual Basic .NET
+' Ubuntu: apt-get install mono-vbnc
+
+Module FizzBuzz
+    Sub Main()
+        Dim I As Int32
+        For I = 1 to 100
+            If I mod 15 = 0 Then
+                Console.WriteLine("FizzBuzz")
+            ElseIf I mod 3 = 0 Then
+                Console.WriteLine("Fizz")
+            ElseIf I mod 5 = 0 Then
+                Console.WriteLine("Buzz")
+            Else
+                Console.WriteLine(I)
+            End If
+        Next
+    End Sub
+End Module
