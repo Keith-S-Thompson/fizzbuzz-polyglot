36233e7f1cbc79f088778697b140090ea40a5a30
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-08-25 12:45:33 -0700

    Add commments. Note that the "nodejs" Ubuntu package breaks this program.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,10 @@
 #!/usr/bin/js
 
+// Language: JavaScript
+// Ubuntu: apt-get install rhino
+// NOTE: Installing the "nodejs" package breaks this program;
+// I'll investigate later.
+
 for (i = 1; i <= 100; i ++) {
     if (i % 15 == 0) {
         print("FizzBuzz")
