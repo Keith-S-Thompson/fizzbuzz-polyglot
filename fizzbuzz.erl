a9a18ee1c5742fa8580c462660fdb4807cd4c601
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-09-01 14:45:55 -0700

    Tweak logic

--- fizzbuzz.erl
+++ fizzbuzz.erl
@@ -13,12 +13,11 @@ print_line(N) when N rem 5 == 0 ->
 print_line(N) ->
     io:format("~w\n", [N]).
 
-print_lines(X, Y) when X > Y ->
-    done;
-print_lines(X, Y) ->
-    % io:format("This is print_lines(~w, ~w)\n", [X, Y]),
+print_lines(X, Y) when Y >= X ->
     print_line(X),
-    print_lines(X+1, Y).
+    print_lines(X+1, Y);
+print_lines(_, _) ->
+    done.
 
 main(_) ->
     print_lines(1, 100).
