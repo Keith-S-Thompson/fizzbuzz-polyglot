aa33b6a11f0ba4aae53620bebfeefcd36e1bfb76
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-09-02 18:27:13 -0700

    Don't depend on "use feature 'say';"

--- fizzbuzz.pl
+++ fizzbuzz.pl
@@ -6,12 +6,11 @@
 
 use strict;
 use warnings;
-use feature 'say';
 
 for (1..100) {
     my $s = '';
     $_%3 or $s.='Fizz';
     $_%5 or $s.='Buzz';
     $s or $s.=$_;
-    say $s;
+    print "$s\n";
 }
