af61f4220d80a0952734a2cc939e791d34036de7
Keith Thompson <Keith.S.Thompson@gmail.com>
2012-09-11 18:09:22 -0700

    Replace if/else with ?:, demonstrating flexibility of function arguments

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -8,16 +8,8 @@
 // I'll investigate later.
 
 for (i = 1; i <= 100; i ++) {
-    if (i % 15 == 0) {
-        print("FizzBuzz")
-    }
-    else if (i % 3 == 0) {
-        print("Fizz")
-    }
-    else if (i % 5 == 0) {
-        print("Buzz")
-    }
-    else {
-        print(i)
-    }
+    print(i % 15 == 0 ? "FizzBuzz" :
+          i %  3 == 0 ? "Fizz" :
+          i %  5 == 0 ? "Buzz" :
+          i);
 }
