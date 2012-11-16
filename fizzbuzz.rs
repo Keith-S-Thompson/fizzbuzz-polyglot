3d900cad141ac5b1ee47d0ecf4b1e343584dd547
Ben Striegel <ben.striegel@gmail.com>
2012-11-16 02:03:25 -0500

    Update fizzbuzz.rs for Rust 0.4

--- fizzbuzz.rs
+++ fizzbuzz.rs
@@ -1,15 +1,13 @@
 // Language: Rust
 // Web site: http://www.rust-lang.org/
 // Ubuntu:   Build compiler from source
-//           I was able to build rust-0.3.1 but not rust-0.4
+//           Should work on 0.4
 
 fn main() {
-    let mut i = 1;
-    while i <= 100 {
-        io::println( if i % 15 == 0 { "FizzBuzz" }
-                     else if i % 3 == 0 { "Fizz" }
-                     else if i % 5 == 0 { "Buzz" }
-                     else { #fmt("%d", i) });
-        i += 1;
+    for int::range(1, 101) |i| {
+        io::println( if i % 15 == 0 { ~"FizzBuzz" }
+                     else if i % 3 == 0 { ~"Fizz" }
+                     else if i % 5 == 0 { ~"Buzz" }
+                     else { fmt!("%d", i) });
     }
 }
