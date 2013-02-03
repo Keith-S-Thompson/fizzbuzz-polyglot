3965c8e05624f63b3fa5acabfcc96f9847aa7e18
Keith Thompson <Keith.S.Thompson@gmail.com>
2013-02-03 14:05:44 -0800

    Update to work with rust-0.5

--- fizzbuzz.rs
+++ fizzbuzz.rs
@@ -1,15 +1,16 @@
 // Language: Rust
 // Web site: http://www.rust-lang.org/
 // Ubuntu:   Build compiler from source
-//           I was able to build rust-0.3.1 but not rust-0.4
+//           I was able to build rust-0.3.1 and rust-0.5 but not rust-0.4
+//           I had to modify the program to work with 0.5
 
 fn main() {
     let mut i = 1;
     while i <= 100 {
-        io::println( if i % 15 == 0 { "FizzBuzz" }
-                     else if i % 3 == 0 { "Fizz" }
-                     else if i % 5 == 0 { "Buzz" }
-                     else { #fmt("%d", i) });
+        if i % 15 == 0 { io::println("FizzBuzz") }
+        else if i % 3 == 0 { io::println("Fizz") }
+        else if i % 5 == 0 { io::println("Buzz") }
+        else { io::println(fmt!("%d", i)) }
         i += 1;
     }
 }
