7b0ced34dc6274004fb6bf70d11fbc7c52968476
Keith Thompson <Keith.S.Thompson@gmail.com>
2013-10-17 15:49:39 -0700

    Simplify bash version

--- fizzbuzz.bash
+++ fizzbuzz.bash
@@ -4,12 +4,12 @@
 # Web site: http://www.gnu.org/software/bash/
 # Ubuntu:   The "bash" package is pre-installed
 
-for (( i=1 ; i <= 100; i ++ )) ; do
-    if (( $i % 15 == 0 )) ; then
+for i in {1..100} ; do
+    if (( i % 15 == 0 )) ; then
         echo FizzBuzz
-    elif (( $i % 3 == 0 )) ; then
+    elif (( i % 3 == 0 )) ; then
         echo Fizz
-    elif (( $i % 5 == 0 )) ; then
+    elif (( i % 5 == 0 )) ; then
         echo Buzz
     else
         echo $i
