540d2f2c39636dde6dedeef4739d2b023e7b09fc
Keith Thompson <Keith.S.Thompson@gmail.com>
2014-04-08 22:57:51 -0700

    Update to Rust 0.10

--- fizzbuzz.rs
+++ fizzbuzz.rs
@@ -1,15 +1,15 @@
 // Language: Rust
 // Web site: http://www.rust-lang.org/
 // Ubuntu:   Build compiler from source
-//           Currently using rust 0.8
+//           Currently using rust 0.10
 
 fn main() {
     let mut i = 1;
     while i <= 100 {
-        if      i % 15 == 0 { println("FizzBuzz") }
-        else if i %  3 == 0 { println("Fizz") }
-        else if i %  5 == 0 { println("Buzz") }
-        else { println(fmt!("%d", i)) }
+        if      i % 15 == 0 { println!("FizzBuzz") }
+        else if i %  3 == 0 { println!("Fizz") }
+        else if i %  5 == 0 { println!("Buzz") }
+        else                { println!("{:d}", i) }
         i += 1;
     }
 }
