54d52736a42ff17deba0b1598cd999166dbe05b9
Keith Thompson <Keith.S.Thompson@gmail.com>
2014-07-27 20:43:52 -0700

    Update Groovy installation instructions

--- fizzbuzz.groovy
+++ fizzbuzz.groovy
@@ -1,6 +1,7 @@
 // Language: Groovy
 // Web site: http://groovy.codehaus.org/
 // Ubuntu:   apt-get install groovy openjdk-6-jdk
+// Linux Mint 7::   apt-get install groovy openjdk-7-jdk
 
 1.upto(100) {
     if (it % 15 == 0) {
