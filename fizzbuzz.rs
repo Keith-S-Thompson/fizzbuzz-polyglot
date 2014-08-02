1c8f94b92370a5905ae240637afe61e055356188
Keith Thompson <Keith.S.Thompson@gmail.com>
2014-08-01 17:00:18 -0700

    Add integer suffix

--- fizzbuzz.rs
+++ fizzbuzz.rs
@@ -1,10 +1,10 @@
 // Language: Rust
 // Web site: http://www.rust-lang.org/
 // Ubuntu:   Build compiler from source
-//           Currently using rust 0.10
+//           Currently using rust 0.11
 
 fn main() {
-    let mut i = 1;
+    let mut i = 1i;
     while i <= 100 {
         if      i % 15 == 0 { println!("FizzBuzz") }
         else if i %  3 == 0 { println!("Fizz") }
