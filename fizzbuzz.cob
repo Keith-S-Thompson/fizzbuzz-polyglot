25709736cbc537e2aca2f7d2156294007ad604cb
Keith Thompson <Keith.S.Thompson@gmail.com>
2014-11-21 00:32:18 -0800

    More verbosity

--- fizzbuzz.cob
+++ fizzbuzz.cob
@@ -8,28 +8,28 @@
         DATA DIVISION.
         WORKING-STORAGE SECTION.
         01 Variables.
-            05 I               PIC 999   VALUE 0.
-            05 Counting        PIC 99    VALUE 0.
-            05 Start-Position  PIC 99    VALUE 0.
-            05 Positions       PIC 99    VALUE 0.
-            05 ignored         PIC S99   VALUE 0.
-            05 I-MOD-15        PIC S99   VALUE 0.
-            05 I-MOD-3         PIC S99   VALUE 0.
-            05 I-MOD-5         PIC S99   VALUE 0.
+            05 I               PIC 999   VALUE ZERO.
+            05 Counting        PIC 99    VALUE ZERO.
+            05 Start-Position  PIC 99    VALUE ZERO.
+            05 Positions       PIC 99    VALUE ZERO.
+            05 ignored         PIC S99   VALUE ZERO.
+            05 I-MOD-15        PIC S99   VALUE ZERO.
+            05 I-MOD-3         PIC S99   VALUE ZERO.
+            05 I-MOD-5         PIC S99   VALUE ZERO.
 
         PROCEDURE DIVISION.
-        PERFORM VARYING I FROM 1 BY 1 UNTIL I > 100
+        PERFORM VARYING I FROM 1 BY 1 UNTIL I IS GREATER THAN 100
 
             DIVIDE I BY 15 GIVING ignored REMAINDER I-MOD-15
-            IF I-MOD-15 = 0
+            IF I-MOD-15 IS EQUAL TO ZERO
                 DISPLAY "FizzBuzz"
             ELSE
                 DIVIDE I BY 3 GIVING ignored REMAINDER I-MOD-3
-                IF I-MOD-3 = 0
+                IF I-MOD-3 IS EQUAL TO ZERO
                     DISPLAY "Fizz"
                 ELSE
                     DIVIDE I BY 5 GIVING ignored REMAINDER I-MOD-5
-                    IF I-MOD-5 = 0
+                    IF I-MOD-5 IS EQUAL TO ZERO
                         DISPLAY "Buzz"
                     ELSE
                         MOVE ZEROES TO Counting
