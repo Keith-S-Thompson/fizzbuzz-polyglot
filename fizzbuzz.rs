1988e8a54af9eeb423adb3d3a08ea776779c8905
Keith Thompson <Keith.S.Thompson@gmail.com>
2015-01-09 16:00:48 -0800

    Update fizzbuzz.rs from rust 0.12 to 1.0.0-alpha

--- fizzbuzz.rs
+++ fizzbuzz.rs
@@ -1,15 +1,15 @@
 // Language: Rust
 // Web site: http://www.rust-lang.org/
 // Ubuntu:   Build compiler from source
-//           Currently using rust 0.12
+//           Currently using rust 1.0.0-alpha
 
 fn main() {
-    let mut i = 1i;
+    let mut i = 1;
     while i <= 100 {
         if      i % 15 == 0 { println!("FizzBuzz") }
         else if i %  3 == 0 { println!("Fizz") }
         else if i %  5 == 0 { println!("Buzz") }
-        else                { println!("{:d}", i) }
+        else                { println!("{}", i) }
         i += 1;
     }
 }
