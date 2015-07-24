877636a49d4b2eb74ac0454d997422a2f876901c
GingersaurusRex <jarrett_coker@yahoo.com>
2015-07-24 18:50:41 -0500

    For loop was wrong
    Endpoint on for-loop is exclusive which means it should be set to end at 101 instead of 100.

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -6,7 +6,7 @@ public class fizzbuzz
 {
     public static final void main( String args[] )
     {
-	for (int i = 1; i <= 100; i ++) {
+	for (int i = 1; i <= 101 i ++) {
             if (i % 15 == 0) {
                 System.out.println("FizzBuzz");
             }
