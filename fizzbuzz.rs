c1c22e5bdb87a9cd1b6a99cbfe2755e807af0b87
Keith Thompson <Keith.S.Thompson@gmail.com>
2015-09-09 18:21:41 -0700

    Rust: Use a for loop

--- fizzbuzz.rs
+++ fizzbuzz.rs
@@ -6,12 +6,10 @@
 //                 (see verify script)
 //                 Currently using rust 1.2.0
 fn main() {
-    let mut i = 1;
-    while i <= 100 {
+    for i in 1..101 {
         if      i % 15 == 0 { println!("FizzBuzz") }
         else if i %  3 == 0 { println!("Fizz") }
         else if i %  5 == 0 { println!("Buzz") }
         else                { println!("{}", i) }
-        i += 1;
     }
 }
