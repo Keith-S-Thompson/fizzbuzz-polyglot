4d3862633c3d4435a0c8dbf7defb7e1a1cee907b
Keith Thompson <Keith.S.Thompson@gmail.com>
2015-12-28 18:21:57 -0800

    Do some cleanup

--- fizzbuzz.rs
+++ fizzbuzz.rs
@@ -2,8 +2,6 @@
 // Web site:       http://www.rust-lang.org/
 // Last tested on: Linux Mint 17.3
 // Requires:       Build compiler from source
-//                 Also need to symlink *.so files and set $LD_LIBRARY_PATH
-//                 (see verify script)
 //                 Currently using rust 1.2.0
 fn main() {
     for i in 1..101 {
