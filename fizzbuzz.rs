24f4ddf0bf602408e96cfa564a8bf65029628d90
Keith Thompson <Keith.S.Thompson@gmail.com>
2016-01-25 17:26:13 -0800

    Using Rust 1.6.0

--- fizzbuzz.rs
+++ fizzbuzz.rs
@@ -1,8 +1,8 @@
 // Language:       Rust
 // Web site:       http://www.rust-lang.org/
 // Last tested on: Linux Mint 17.3
-// Requires:       Build compiler from source
-//                 Currently using rust 1.2.0
+// Requires:       Build compiler from source and update ldconfig
+//                 Currently using rust 1.6.0
 fn main() {
     for i in 1..101 {
         if      i % 15 == 0 { println!("FizzBuzz") }
