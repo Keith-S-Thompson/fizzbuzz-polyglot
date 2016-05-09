d9c32a17845fde4ff09b2260a43bbc3bd8b37e1f
Keith Thompson <Keith.S.Thompson@gmail.com>
2016-05-08 19:13:55 -0700

    fizzbuzz.lua: Remove semicolons

--- fizzbuzz.lua
+++ fizzbuzz.lua
@@ -9,11 +9,11 @@
 
 for i = 1, 100 do
     if i % 15 == 0 then
-        print("FizzBuzz");
+        print("FizzBuzz")
     elseif i % 3 == 0 then
-        print("Fizz");
+        print("Fizz")
     elseif i % 5 == 0 then
-        print("Buzz");
+        print("Buzz")
     else
         print(i)
     end
