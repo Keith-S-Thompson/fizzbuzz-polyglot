0ae0ad3d82005022f736d6ffdf90c2863a26cc9e
Keith Thompson <Keith.S.Thompson@gmail.com>
2016-09-29 17:39:22 -0700

    Insignificant comment change

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -4,7 +4,7 @@
 // Requires:       apt-get install golang
 
 // NOTE: The above currently installs Go version 1.2.1.
-//       The latest version is 1.7, installable from 
+//       The latest version is 1.7, installable from
 //       https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz
 
 package main
