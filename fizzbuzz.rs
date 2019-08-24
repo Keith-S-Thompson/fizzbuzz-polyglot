9f6d5b4e8ce1ab40c442e2d6d302af4058880732
Keith Thompson <Keith.S.Thompson@gmail.com>
2019-08-24 00:28:32 -0700

    Rust: Use inclusive range

--- fizzbuzz.rs
+++ fizzbuzz.rs
@@ -3,7 +3,7 @@
 // Last tested on: Ubuntu 18.04
 // Requires:       apt-get install rustc
 fn main() {
-    for i in 1..101 {
+    for i in 1..=100 {
         if      i % 15 == 0 { println!("FizzBuzz") }
         else if i %  3 == 0 { println!("Fizz") }
         else if i %  5 == 0 { println!("Buzz") }
