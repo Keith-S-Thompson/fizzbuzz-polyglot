59ebe74799d419678f461c99db575de749bdadf9
Keith Thompson <Keith.S.Thompson@gmail.com>
2024-07-28 16:14:52 -0700

    fizzbuzz.zig - Update to zig 0.14.0

--- fizzbuzz.zig
+++ fizzbuzz.zig
@@ -1,34 +1,26 @@
 // Language:       Zig
 // Web site:       https://ziglang.org/
-// Last tested on: Ubuntu 22.04.4
-// Requires:       Install binaries from
-//     https://ziglang.org/download/0.4.0/zig-linux-x86_64-0.4.0.tar.xz
+// Last tested on: Ubuntu 22.04.3
+// Requires:       sudo snap install --beta --classic zig
+//                 (Installs zig 0.13.0)
 
 const std = @import("std");
 
 pub fn main() !void {
-    var stdout_file = try std.io.getStdOut();
+    const stdout = std.io.getStdOut().writer();
     var i: u32 = 1;
-    while (i <= 100) {
+    while (i <= 100) : (i += 1) {
         if (i % 15 == 0) {
-            try stdout_file.write("FizzBuzz\n");
+            try stdout.writeAll("FizzBuzz\n");
         }
         else if (i % 3 == 0) {
-            try stdout_file.write("Fizz\n");
+            try stdout.writeAll("Fizz\n");
         }
         else if (i % 5 == 0) {
-            try stdout_file.write("Buzz\n");
+            try stdout.writeAll("Buzz\n");
         }
         else {
-            // There should be a cleaner way to do this.
-            var line: [5]u8 = undefined;
-            var len: usize = 3;
-            if (i > 9) {
-                len = 4;
-            }
-            const x = try std.fmt.bufPrint(line[0..], "{}\n", i);
-            try stdout_file.write(line[0..len-1]);
+            try stdout.print("{d}\n", .{i});
         }
-        i += 1;
     }
 }
