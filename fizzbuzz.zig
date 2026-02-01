f9fa380a01326365f6a2dff92cf96fd6d2ba674d
Keith Thompson <Keith.S.Thompson@gmail.com>
2026-01-31 16:17:10 -0800

    Update fizzbuzz.zig
    Zig has yet again changed the way you write simple messages to
    standard output.

--- fizzbuzz.zig
+++ fizzbuzz.zig
@@ -2,12 +2,12 @@
 // Web site:       https://ziglang.org/
 // Last tested on: Ubuntu 22.04.3
 // Requires:       sudo snap install --beta --classic zig
-//                 (Installs zig 0.13.0)
+//                 (Installs zig 0.15.2)
 
 const std = @import("std");
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
+    const stdout = std.fs.File.stdout();
     var i: u32 = 1;
     while (i <= 100) : (i += 1) {
         if (i % 15 == 0) {
@@ -20,7 +20,9 @@ pub fn main() !void {
             try stdout.writeAll("Buzz\n");
         }
         else {
-            try stdout.print("{d}\n", .{i});
+            var buf: [3]u8 = undefined;
+            const str = try std.fmt.bufPrint(&buf, "{}\n", .{i});
+            try stdout.writeAll(str);
         }
     }
 }
